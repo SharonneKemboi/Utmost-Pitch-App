268d04a7cc467f8086ed841d5918d4a168585161
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-09 19:09:31 +0300

    Create function that sends email to new users

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,9 @@
 from . import auth
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from app.models import User
 from flask import render_template
+from flask_login import login_user
+from app.email import create_mail
 
 @auth.route("/", methods = ["GET","POST"])
 def register():
@@ -14,4 +16,25 @@ def register():
 
         new_user = User(name = name, email = email, password = pass_input)
         new_user.save_user()
+        create_mail("Hey","email/email",new_user.email,name = new_user.name)
+
+
     return render_template("auth/login.html",form = form)
+
+
+@auth.route("/logged", methods = ["GET","POST"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user_email = form.email.data
+        user_password = form.password.data
+        remember = form.remember_me.data
+
+        user = User.query.filter_by(email = user_email).first()
+
+        if user is not None and user.verify_pass(user_password):
+            login_user(user,remember)
+            return render_template("loggedin.html", user = user)
+
+    return render_template("auth/login.html",form = form)    
