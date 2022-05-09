b21c54b9589be9a56a8f875c7becc5d752f9f5de
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-09 16:35:40 +0300

    Update config classes

--- config.py
+++ config.py
@@ -5,27 +5,48 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:New Password@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Password@localhost/pitches'
+    UPLOADED_PHOTOS_DEST = "app/static/assets"
+    
 
-
-   
-
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
+    #email stuff
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("EMAIL_USERNAME")
+    MAIL_PASSWORD  = os.environ.get("EMAIL_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Password@localhost/pitches'
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
+    """
+    This is the class which we will use to set the configurations during development stage of the app
     Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:New Password@localhost/pitches'
-
+        COnfig - this is the parent config class from which we inherit its properties
+    """
     DEBUG = True
 
+
+class ProdConfig(Config):
+    """
+    This is the class which we will use to set the configurations during production stage of the app
+    Args:
+        Config - this is the parent config class from which we inherit its properties
+    """
+    pass
+
+
+class TestConfig(Config):
+    """
+    This is the class which we will use to set the configurations during testing stage of the app
+    Args:
+        Config - this is the parent config class from which we inherit its properties
+    """
+    pass
+
+
+config_options = {
+    "test": TestConfig,
+    "production": ProdConfig,
+    "development": DevConfig
+} 
