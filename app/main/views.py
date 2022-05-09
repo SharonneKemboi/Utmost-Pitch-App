4d1aa9e8ac276c37d8a5854245edd5acf783952d
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-09 19:28:59 +0300

    create a addcomment class that will allow users to add comments

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from ..models import User,Pitch,Comment
-from .forms import AddPitchForm
+from .forms import AddPitchForm,AddComment
 from . import main
 
 
@@ -20,3 +20,24 @@ def index(uname):
         return render_template("pitched.html", pitches = pitches)
 
     return render_template("index.html", form = form, title = title) 
+
+
+@main.route("/<user>/pitch/<pitch_id>/add/comment", methods = ["GET","POST"])
+def comment(user,pitch_id):
+    user = User.query.filter_by(id = user).first()
+    pitch = Pitch.query.filter_by(id = pitch_id).first()
+    form = AddComment()
+
+    if form.validate_on_submit():
+        content = form.content.data
+        new_comment = Comment(content = content, user = user, pitch = pitch )
+        new_comment.save_comment()
+    return render_template("comment.html", title = pitch.title,form = form)
+
+@main.route("/<pitch_id>/comments")
+def view_comments(pitch_id):
+    pitch = Pitch.query.filter_by(id = pitch_id).first()
+
+    comments = pitch.get_pitch_comments()
+
+    return render_template("view.html", comments = comments)
