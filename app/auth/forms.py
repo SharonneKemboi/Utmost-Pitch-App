4d913b47a8f2dc572ea294bf7e55f91c9f2eb329
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-09 19:00:42 +0300

    Create a Login Class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, PasswordField,ValidationError
+from wtforms import SubmitField, StringField, PasswordField,ValidationError,BooleanField
 from wtforms.validators import DataRequired, EqualTo, Email
 from app.models import User
 
-
+#first class
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators = [DataRequired()])
     email = StringField("Email", validators = [DataRequired(), Email()])
@@ -19,3 +19,11 @@ class RegistrationForm(FlaskForm):
     def validate_email(self,data_field):
         if User.query.filter_by(email = data_field.data).first():
             raise ValidationError("This account already exists")   
+
+
+#second class
+class LoginForm(FlaskForm):
+    email = StringField("Email",validators=[DataRequired(),Email()])
+    password = PasswordField("Password", validators = [DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Login")            
