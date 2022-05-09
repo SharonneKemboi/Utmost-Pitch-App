67885a74480d265208cb58e8620b0ebd93a2d398
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-09 16:40:45 +0300

    create classes in the model and update in manage file

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role
+from app.models import User,Comment,Pitch
 
 
 #Creating app instance
@@ -13,7 +13,14 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Role = Role )
+   return dict(db = db, app = app, User = User, Pitch = Pitch, )
 
 if __name__ == '__main__':
     manager.run()
+
+
+
+
+
+
+
